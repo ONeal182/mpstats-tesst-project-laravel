5f97b009d6a20770938c1701f8b418e9d12237ce
ONeal <kigat63@gmail.com>
2022-03-21 16:43:34 +0400

    fix bug list

--- resources/views/compilationlist.blade.php
+++ resources/views/compilationlist.blade.php
@@ -13,7 +13,7 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-                        
+
                         <table class="table">
                             <thead>
                                 <tr>
@@ -21,30 +21,35 @@
                                     <th scope="col">Название</th>
                                     <th scope="col">Название товаров</th>
                                     <th scope="col" style="    display: flex;
-                                    justify-content: flex-end;">
+                                        justify-content: flex-end;">
                                         <a class="btn btn-success" href="/mpstats/compilation/add">Новая подборка</a>
                                     </th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($data as $key => $value)
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>{{$value->title}}</td>
-                                        <td>
-                                            {{-- {{$value->data[0]->item->id}} --}}
-                                            @if ($value->data != null)
-                                            @foreach ($value->data as $product)
-                                            {{$product->item->name}}<br>
-                                        @endforeach
-                                            @endif
-                                            
-                                        </td>
-                                        <td class="d-flex" ><a class="btn btn-primary" href="/mpstats/compilation/{{$value->id}}">Посмотреть</a>
-                                        <a class="btn btn-danger "href="/mpstats/compilation/delete/{{$value->id}}">Удалить</a>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                @if ($data != null)
+                                    @foreach ($data as $key => $value)
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>{{ $value->title }}</td>
+                                            <td>
+                                                {{-- {{$value->data[0]->item->id}} --}}
+                                                @if ($value->data != null)
+                                                    @foreach ($value->data as $product)
+                                                        {{ $product->item->name }}<br>
+                                                    @endforeach
+                                                @endif
+
+                                            </td>
+                                            <td class="d-flex"><a class="btn btn-primary"
+                                                    href="/mpstats/compilation/{{ $value->id }}">Посмотреть</a>
+                                                <a class="btn btn-danger "
+                                                    href="/mpstats/compilation/delete/{{ $value->id }}">Удалить</a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+
                             </tbody>
                         </table>
                     </div>
