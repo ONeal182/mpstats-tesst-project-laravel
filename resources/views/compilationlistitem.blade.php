a229b3a78d0787ba99b8d112acde184e898cc5bd
root <root@498611-sensei2769.tmweb.ru>
2022-03-24 12:51:33 +0300

    added modal

--- resources/views/compilationlistitem.blade.php
+++ resources/views/compilationlistitem.blade.php
@@ -80,7 +80,7 @@
 
         <div class="col-7 graphs-block">
           <div class="graphs-block__header">
-            <div>
+            <div class="graphs-block__header-block">
               <h3 class="graphs-block__title">Сводная информация</h3>
               <div class="graphs-block__question" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
                 <svg width="20" height="20" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -90,27 +90,37 @@
                 </svg>
               </div>
             </div>
-            <div>
-              <button class="graphs-block__btn__last-mounth">Прошлый месяц</button>
+            <div class="graphs-block__header-block">
+              <button class="graphs-block__btn__last-mounth mr-16">Прошлый месяц</button>
+
+              <div class="datapicker-wrap mr-16">
+                <input type="text" class="datepicker-here" data-min-view="months" data-view="months" data-date-format="M yyyy" />
+
+                <svg class="svg-calendar" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M1.75 3.5C1.75 2.85567 2.27233 2.33333 2.91667 2.33333H11.0833C11.7277 2.33333 12.25 2.85567 12.25 3.5V11.6667C12.25 12.311 11.7277 12.8333 11.0833 12.8333H2.91667C2.27233 12.8333 1.75 12.311 1.75 11.6667V3.5Z" stroke="#414BB2" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round" />
+                  <path d="M9.33325 1.16667V3.5" stroke="#414BB2" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round" />
+                  <path d="M4.66675 1.16667V3.5" stroke="#414BB2" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round" />
+                  <path d="M1.75 5.83333H12.25" stroke="#414BB2" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </div>
+
+              <button class="graphs-block__btn__show-more">
+                <svg width="5" height="4" viewBox="0 0 5 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M2.33341 3.16667C2.97775 3.16667 3.50008 2.64433 3.50008 2C3.50008 1.35567 2.97775 0.833333 2.33341 0.833333C1.68908 0.833333 1.16675 1.35567 1.16675 2C1.16675 2.64433 1.68908 3.16667 2.33341 3.16667Z" stroke="#414BB2" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <svg width="5" height="4" viewBox="0 0 5 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M2.33341 3.16667C2.97775 3.16667 3.50008 2.64433 3.50008 2C3.50008 1.35567 2.97775 0.833333 2.33341 0.833333C1.68908 0.833333 1.16675 1.35567 1.16675 2C1.16675 2.64433 1.68908 3.16667 2.33341 3.16667Z" stroke="#414BB2" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <svg width="5" height="4" viewBox="0 0 5 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M2.33341 3.16667C2.97775 3.16667 3.50008 2.64433 3.50008 2C3.50008 1.35567 2.97775 0.833333 2.33341 0.833333C1.68908 0.833333 1.16675 1.35567 1.16675 2C1.16675 2.64433 1.68908 3.16667 2.33341 3.16667Z" stroke="#414BB2" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+
+              </button>
             </div>
 
-            <input type="text" class="datepicker-here" data-min-view="months" data-view="months" data-date-format="M yyyy" />
-
-            <button class="graphs-block__btn__show-more">
-              <svg width="5" height="4" viewBox="0 0 5 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M2.33341 3.16667C2.97775 3.16667 3.50008 2.64433 3.50008 2C3.50008 1.35567 2.97775 0.833333 2.33341 0.833333C1.68908 0.833333 1.16675 1.35567 1.16675 2C1.16675 2.64433 1.68908 3.16667 2.33341 3.16667Z" stroke="#414BB2" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-              <svg width="5" height="4" viewBox="0 0 5 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M2.33341 3.16667C2.97775 3.16667 3.50008 2.64433 3.50008 2C3.50008 1.35567 2.97775 0.833333 2.33341 0.833333C1.68908 0.833333 1.16675 1.35567 1.16675 2C1.16675 2.64433 1.68908 3.16667 2.33341 3.16667Z" stroke="#414BB2" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-              <svg width="5" height="4" viewBox="0 0 5 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M2.33341 3.16667C2.97775 3.16667 3.50008 2.64433 3.50008 2C3.50008 1.35567 2.97775 0.833333 2.33341 0.833333C1.68908 0.833333 1.16675 1.35567 1.16675 2C1.16675 2.64433 1.68908 3.16667 2.33341 3.16667Z" stroke="#414BB2" stroke-width="1.16667" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-
-            </button>
           </div>
 
-          <div id="main" style="width: 600px;height:400px;"></div>
+          <div id="main" style="width: 100%;height:300px;"></div>
 
 
         </div>
@@ -263,18 +273,18 @@
               <td>
                 <img class="my-product__img" src="/img/unknow.jpg" alt="product-img">
               </td>
-              <td class="col-4"> <a class="sales-data__table-link">Пирамида игра для кошки / Интерактивная игрушка для кошек баш..</a></td> <!-- name -->
+              <td class="col-4"> <a class="sales-data__table-link">Кардиган Gun</a></td> <!-- name -->
               <td class="col-2 w-20">
-                Wildberries
+                Ozon
                 <!-- marketplace -->
-                <p class="txt-gray">Игрушки для животных</p> <!-- category -->
+                <p class="txt-gray">Одежда</p> <!-- category -->
               </td>
               <td>5</td> <!-- rating -->
               <td>2</td><!-- position -->
               <td class="col-2">
-                Антицарапки
+                Nike
                 <!-- brand -->
-                <p class="txt-gray">ИП Каплин Анатол...</p><!-- seller -->
+                <p class="txt-gray">Nike</p><!-- seller -->
               </td>
               <td class="col-1">
                 829 170 &#8381;
@@ -282,7 +292,9 @@
               </td>
               <td class="col-1"></td><!-- graph -->
             </tr>
+
             <tr class="empty-tr"></tr>
+
             <tr>
               <td>
                 <img class="my-product__img" src="/img/unknow.jpg" alt="product-img">
@@ -306,7 +318,9 @@
               </td>
               <td class="col-1"></td><!-- graph -->
             </tr>
+
             <tr class="empty-tr"></tr>
+
             <tr>
               <td>
                 <img class="my-product__img" src="/img/unknow.jpg" alt="product-img">
@@ -334,7 +348,7 @@
           </tbody>
         </table>
 
-        <button class="btn btn-unique btn-rounded btn-sm my-0 add-new-product purple-button">
+        <button class="btn btn-unique btn-rounded btn-sm my-0 add-new-product purple-button" data-toggle="modal" data-target="#exampleModal">
           <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M9 16.5C13.1421 16.5 16.5 13.1421 16.5 9C16.5 4.85786 13.1421 1.5 9 1.5C4.85786 1.5 1.5 4.85786 1.5 9C1.5 13.1421 4.85786 16.5 9 16.5Z" stroke="#414BB2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
             <path d="M9 6V12" stroke="#414BB2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -353,6 +367,65 @@
 </div>
 @endsection
 
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Добавить товар</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="name" class="col-form-label">Название</label>
+            <input type="text" class="form-control" id="name">
+          </div>
+          <div class="form-group">
+            <label for="marketplace" class="col-form-label">Маркетплейс</label>
+            <input type="text" class="form-control" id="marketplace">
+          </div>
+          <div class="form-group">
+            <label for="category" class="col-form-label">Категория</label>
+            <input type="text" class="form-control" id="category">
+          </div>
+
+          <div class="form-group">
+            <label for="raiting" class="col-form-label">Рейтинг</label>
+            <input type="number" class="form-control" id="raiting">
+          </div>
+          <div class="form-group">
+            <label for="position" class="col-form-label">Позиция</label>
+            <input type="number" class="form-control" id="position">
+          </div>
+          <div class="form-group">
+            <label for="brand" class="col-form-label">Бренд</label>
+            <input type="text" class="form-control" id="brand">
+          </div>
+          <div class="form-group">
+            <label for="saller" class="col-form-label">Продавец</label>
+            <input type="text" class="form-control" id="saller">
+          </div>
+
+          <div class="form-group">
+            <label for="money" class="col-form-label">Выручка</label>
+            <input type="number" class="form-control" id="money">
+          </div>
+          <div class="form-group">
+            <label for="count" class="col-form-label">Продаж</label>
+            <input type="number" class="form-control" id="count">
+          </div>
+         
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+        <button type="button" class="btn btn-primary">Добавить</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <!-- <div class="container">
   <div class="row justify-content-center">
