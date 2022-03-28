1775d9068145cfa3263d7a9eb06dbe61b99fd023
root <root@498611-sensei2769.tmweb.ru>
2022-03-28 11:08:01 +0300

    mobile adaptive for breadcrumbs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,7 +119,7 @@
                         </a>
                     </div> --}}
                 </div>
-                <div class="col-4">
+                <!-- <div class="col-4">
                     <div class="left-block_wrapper">
                         {{-- <div class="btn-wrapper">
                             <input class="custom-btn" type="button" name="" value="Проанализировать товар">
@@ -133,7 +133,7 @@
                             </a>
                         </div> --}}
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
 
@@ -147,3 +147,56 @@
 </body>
 
 </html>
+
+<!-- <div class="container">
+    <a class="navbar-brand" href="{{ url('/') }}">
+    {{-- <img class="header-logo" src="img/logo-header.png" alt="logo"> --}}
+        {{ config('app.name', 'Laravel') }}
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        
+        <ul class="navbar-nav me-auto">
+
+        </ul>
+
+        
+        <ul class="navbar-nav ms-auto">
+    
+            @guest
+                @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                @endif
+
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul>
+    </div>
+</div> -->
